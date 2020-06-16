a243a6d2c58aca237cd820d1821c8045aaa9249b
Rissap <rinej.sapset@gmail.com>
2020-06-16 10:49:50 +0300

    dev: youtube api query done

--- django-app/main/apps.py
+++ django-app/main/apps.py
@@ -1,7 +1,6 @@
 from django.apps import AppConfig
-from googleapiclient.discovery import build
-import api_token
 
 
 class MainConfig(AppConfig):
     name = 'main'
+    
--- django-app/search/views.py
+++ django-app/search/views.py
@@ -1,10 +1,23 @@
 from django.shortcuts import render
 from django.views.generic import TemplateView
 
-from main.apps import *
+from googleapiclient.discovery import build
+import api_token
+
+#youtube api requests
+def get_test(youtube):
+    request = youtube.channels().list(
+        part="statistics",
+        forUsername="shafer5"
+    )
+    response = request.execute()
+    print(response)
+    return response
+
 
 class Search(TemplateView):
 	template_name = "search.html"
+	youtube = build('youtube', 'v3', developerKey=api_token.token)
 
 	def get(self, request):
 		args = {}
@@ -12,6 +25,6 @@ class Search(TemplateView):
 
 	def post(self, request):
 		query = request.POST["search"]
-		print(query)
+		response = get_test(self.youtube)
 		args = {"query": query}
 		return render(request, self.template_name, args)
